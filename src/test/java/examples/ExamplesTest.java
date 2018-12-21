96285290c22e43c8576d5b1e246c67e5afb702ec
a.mercier <a.mercier-external@overkiz.com>
2018-12-21 13:50:45 +0100

    reproducing bug

--- src/test/java/examples/ExamplesTest.java
+++ src/test/java/examples/ExamplesTest.java
@@ -1,11 +1,35 @@
 package examples;
 
-import com.intuit.karate.junit4.Karate;
-import org.junit.runner.RunWith;
+import com.intuit.karate.cucumber.CucumberRunner;
+import com.intuit.karate.cucumber.KarateStats;
+import cucumber.api.CucumberOptions;
+import java.io.File;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
-@RunWith(Karate.class)
+@CucumberOptions(tags = {"~@ignore"})
 public class ExamplesTest {
     // this will run all *.feature files that exist in sub-directories
-    // refer to https://github.com/intuit/karate#naming-conventions
-    // for folder-structure recommendations and naming conventions
+    @Test
+    public void testParallel() {
+        String karateOutputPath = "target/surefire-reports";
+
+        KarateStats stats = CucumberRunner.parallel(getClass(), 10, "target/surefire-reports");
+        generateReport(karateOutputPath);
+        assertTrue("there are scenario failures", stats.getFailCount() == 0);
+    }
+
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
+        List<String> jsonPaths = jsonFiles.stream().map(File::getAbsolutePath).collect(Collectors.toList());
+        Configuration config = new Configuration(new File("target"), "api");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
 }
